0c6715a370f453f1ce764b871765646a0242e6e5
Anusha <anushaboda563@gmail.com>
2026-07-27 23:44:32 +0000

    fix api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://customer-relationship-management-tzoy.onrender.com",
+  baseURL: "https://customer-relationship-management-tzoy.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    return config;
+  return config;
 });
 
 export default api;
